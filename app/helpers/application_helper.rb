0e26af6e5562a40a2a78571e0fb4422a2540bfca
Mark Merritt <markmerritt804@gmail.com>
2019-02-14 06:13:53 -0500

    Add contact bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def display_icon(svg_name, style_class = "")
+    path = asset_path('sprite.svg') 
+    svg_name = svg_name.prepend("#icon-")
+    
+    content_tag(:svg, class: style_class) do 
+      content_tag(:use, nil, 'xlink:href' => path + svg_name)
+    end
+  end
 end
