698ff7d54356e0f517e84df1e6a9390be7127cbc
Mark Merritt <markmerritt804@gmail.com>
2019-02-20 18:32:23 -0500

    Add rest of User seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,34 @@
+User.delete_all
+
+employee_user = User.new(
+  first_name: "Employee",
+  last_name: "User", 
+  role: :employee,
+  email: "employee@example.com",
+  password: "helloworld",
+  password_confirmation: "helloworld"
+)
+
+employee_user.skip_confirmation!
+employee_user.save!
+
+puts "1 Employee User Created"
+
+supervisor_user = User.new(
+  first_name: "Supervisor",
+  last_name: "User", 
+  role: :supervisor,
+  email: "supervisor@example.com",
+  password: "helloworld",
+  password_confirmation: "helloworld"
+)
+
+supervisor_user.skip_confirmation!
+supervisor_user.save!
+
+puts "1 Supervisor User Created"
+
+
 admin_user = User.new(
   first_name: "Admin",
   last_name: "User", 
@@ -11,3 +42,4 @@ admin_user.skip_confirmation!
 admin_user.save!
 
 puts "1 Admin User Created"
+
