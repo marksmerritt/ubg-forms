47ecc1b1358693e4e20e320f456158fe84c48a1a
Mark Merritt <markmerritt804@gmail.com>
2019-02-20 18:27:35 -0500

    Add rest of user factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,25 @@ FactoryBot.define do
     "user#{n}@example.com"
   end
 
+  factory :employee_user, class: User do
+    first_name { "Employee" }
+    last_name { "User" }
+    email { generate :email }
+    password { "helloworld" }
+    password_confirmation { "helloworld" }
+    confirmed_at { Date.yesterday }
+  end
+
+  factory :supervisor_user, class: User do
+    first_name { "Supervisor" }
+    last_name { "User" }
+    role { :supervisor }
+    email { generate :email }
+    password { "helloworld" }
+    password_confirmation { "helloworld" }
+    confirmed_at { Date.yesterday }
+  end
+
   factory :admin_user, class: User do
     first_name { "Admin" }
     last_name { "User" }
