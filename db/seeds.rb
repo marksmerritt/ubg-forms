301b2e2e32087a6f5ec2431e5689dc86db97d6ac
Mark Merritt <markmerritt804@gmail.com>
2019-02-25 22:02:33 -0500

    Add seeds for all models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,14 @@
+require 'faker'
+
+# -------------------- Reset --------------------
+
+
+Form.delete_all
+FormField.delete_all
+FormType.delete_all
 User.delete_all
+
+# -------------------- Users --------------------
 
 employee_user = User.new(
   first_name: "Employee",
@@ -12,7 +22,6 @@ employee_user = User.new(
 employee_user.skip_confirmation!
 employee_user.save!
 
-puts "1 Employee User Created"
 
 supervisor_user = User.new(
   first_name: "Supervisor",
@@ -25,8 +34,6 @@ supervisor_user = User.new(
 
 supervisor_user.skip_confirmation!
 supervisor_user.save!
-
-puts "1 Supervisor User Created"
 
 
 admin_user = User.new(
@@ -41,5 +48,114 @@ admin_user = User.new(
 admin_user.skip_confirmation!
 admin_user.save!
 
-puts "1 Admin User Created"
+
+50.times do 
+  employee_user = User.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name, 
+    role: :employee,
+    email: Faker::Internet.email,
+    password: "helloworld",
+    password_confirmation: "helloworld"
+  )
+  employee_user.skip_confirmation!
+  employee_user.save!
+end
+
+@users = User.all
+
+puts "#{User.count} Users created"
+
+
+# -------------------- Form Types ---------------------
+
+form_types = ["Scaffold Inspection", "Forklift Inspection", "Boom/Scissor Inspection",
+              "Daily Mast Climber Work Platform Scaffold Check List", "Mixer Inspection",
+              "Ladder Inspection", "Request For Accident Information", "Job Hazard Analysis"]
+
+form_types.each do |form_type|
+  FormType.create!(
+    name: form_type
+  )
+end
+
+@form_types = FormType.all
+
+puts "#{FormType.count} Form Types created"
+
+
+# -------------------- Form Fields --------------------
+
+field_type_options = FormField::FIELD_TYPE_OPTIONS
+
+50.times do 
+  FormField.create!(
+    name: Faker::Lorem.word,
+    field_type: field_type_options.sample,
+    required: Faker::Boolean.boolean,
+    form_type: @form_types.sample
+  )
+end
+
+@form_fields = FormField.all
+
+puts "#{FormField.count} Form Fields created"
+
+
+# -------------------- Forms --------------------------
+
+500.times do 
+  @form_type = @form_types.sample
+  @properties = {}
+
+  @form_type.fields.each do |field|
+    if field.field_type == "text_field"
+      @properties[field.name] = Faker::Lorem.word
+    elsif field.field_type == "text_area"
+      @properties[field.name] = Faker::Lorem.sentence
+    elsif field.field_type == "check_box"
+      @properties[field.name] = [0,1].sample
+    end
+  end
+
+  Form.create!(
+    form_type: @form_type,
+    user: @users.sample,
+    properties: @properties
+  )
+end
+
+@forms = Form.all
+
+puts "#{Form.count} Forms created"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
