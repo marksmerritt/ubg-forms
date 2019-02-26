22e50e30e7dbd84461366511a7a6a45ce54c4746
Mark Merritt <markmerritt804@gmail.com>
2019-02-25 22:05:22 -0500

    Remove unnecessary spec

--- spec/features/portal/form_spec.rb
+++ spec/features/portal/form_spec.rb
@@ -1,18 +1,5 @@
 require "rails_helper"
 
 describe "navigation" do
-  let(:user) { FactoryBot.create(:user) }
 
-  before do 
-    login_as(user, :scope => :user)
-  end 
-
-  describe "overview" do 
-    let(:form) { FactoryBot.create(:form, user: user) }
-
-    it "displays a list of recently submitted forms by the current user" do 
-      visit form_overview_path
-      expect(page).to have_content(form.properties.values.first)
-    end
-  end
 end
