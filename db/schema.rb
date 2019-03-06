e47d674964670976f8d1d208ac125608fed5d302
Mark Merritt <markmerritt804@gmail.com>
2019-03-06 15:15:02 -0500

    Remove cols/rows from checklist in favor of checklist type

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_06_184331) do
+ActiveRecord::Schema.define(version: 2019_03_06_201253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "checklists", force: :cascade do |t|
-    t.text "col", default: [], array: true
-    t.text "row", default: [], array: true
     t.jsonb "properties", default: {}, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
