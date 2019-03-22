5b61a3ecd529f6d33261794addc2f39516613853
Mark Merritt <markmerritt804@gmail.com>
2019-03-22 13:01:56 -0400

    Add form table links

--- app/controllers/portal/form_overviews_controller.rb
+++ app/controllers/portal/form_overviews_controller.rb
@@ -1,6 +1,6 @@
 class Portal::FormOverviewsController < Portal::BaseController
   def show
     @form_types = FormType.all
-    @forms = Form.includes(:user, :form_type).order("updated_at DESC")
+    @forms = Form.includes(:user, :form_type).order("updated_at DESC").limit(25)
   end
 end
