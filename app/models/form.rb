c18c02f72f9cf7d3a13a4f757157aea0ff47c4ee
Mark Merritt <markmerritt804@gmail.com>
2019-03-30 16:32:08 -0400

    Validate all form fields provided

--- app/models/form.rb
+++ app/models/form.rb
@@ -5,4 +5,16 @@ class Form < ApplicationRecord
   belongs_to :user
 
   validates_presence_of :job_number
+
+  validate :all_fields_present
+
+  def all_fields_present
+    if has_empty_fields?(header_properties) || has_empty_fields?(checklist_properties) || has_empty_fields?(footer_properties)
+      errors.add(:base, "Please fill out all fields")
+    end
+  end
+
+  def has_empty_fields?(fields)
+    fields.values.any?{ |field| field.empty? }
+  end
 end
