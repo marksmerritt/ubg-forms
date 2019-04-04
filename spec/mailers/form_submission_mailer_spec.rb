e385efbc48bb08e1a74781383ea1a0ba4b866540
Mark Merritt <markmerritt804@gmail.com>
2019-04-04 19:36:50 -0400

    Add FormSubmissionMailer spec

--- spec/mailers/form_submission_mailer_spec.rb
+++ spec/mailers/form_submission_mailer_spec.rb
@@ -1,5 +1,14 @@
 require "rails_helper"
 
 RSpec.describe FormSubmissionMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user) }
+  let(:form) { FactoryBot.create(:form, user: user) }
+
+  describe "#email_form" do 
+    it "sends the email" do 
+      FormSubmissionMailer.email_form(form, "", form.user).deliver_now
+
+      expect(ActionMailer::Base.deliveries).to_not be_empty
+    end
+  end
 end
