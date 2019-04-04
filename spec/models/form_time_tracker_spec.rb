4db59f77e472353a284c1466363a510c92bcf058
Mark Merritt <markmerritt804@gmail.com>
2019-04-04 19:50:46 -0400

    Add form time taken spec

--- spec/models/form_time_tracker_spec.rb
+++ spec/models/form_time_tracker_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe FormTimeTracker, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "time_taken" do 
+    it "sets the correct time taken on creation" do 
+      @form_time = FactoryBot.create(:form_time_tracker, 
+                                     new_impression_at: 10.minutes.ago,
+                                     create_impression_at: Time.now)
+
+      expect(@form_time.time_taken.floor).to eq(600)
+    end
+  end
 end
+
