3541cdb16d2974d3b7d79e485fde225d836d4047
Mark Merritt <markmerritt804@gmail.com>
2019-04-07 17:44:34 -0400

    Store form images in same Azure dir as forms

--- lib/azure_helper.rb
+++ lib/azure_helper.rb
@@ -1,4 +1,4 @@
-# TODO: Store Uploaded Versions In Same Directory
+# TODO: Refactor!
 
 require "pdf_generator"
 
@@ -8,26 +8,42 @@ module AzureHelper
                                              storage_access_key: ENV["AZURE_STORAGE_ACCESS_KEY"])
   end
 
-  def self.generate_filename(form)
-    form_type = "#{form.form_type.name.gsub(' ', '-').downcase!}s"
-    
-    "#{form_type}/job##{form.job_number}--#{form.created_at.strftime('%Y-%m-%d')}--#{form.id}"
+  def self.generate_dir(form)
+    "#{form.form_type.name.gsub(' ', '-').downcase!}s"
+  end
+
+  def self.generate_filename(form:, dir:, content_type:, index: nil) 
+    base = "#{dir}/job##{form.job_number}--#{form.created_at.strftime('%Y-%m-%d')}--#{form.id}"
+
+    case content_type
+      when "form"
+        base += "/form" 
+      when "image"
+        base += ("/image" + "#{index + 1}")
+      else
+        base += "/unk"
+    end
+
+    base
   end
 
   def self.send_form(form)
     client = create_client_instance
     container = set_container_name
-    filename = generate_filename(form)
+    dir = generate_dir(form)
+    filename = generate_filename(form: form, dir: dir, content_type: "form") 
     content = PdfGenerator.generate(form)
 
     client.create_block_blob(container, filename, content)
+    send_images_with_form(client, form, container, dir) if form.images.attached?
     send_failure_email(form, content) if form.has_failures?
   end
 
   def self.send_uploaded_form(form_upload)
     client = create_client_instance
     container = set_container_name
-    filename = generate_filename(form_upload)
+    dir = generate_dir(form_upload)
+    filename = generate_filename(form: form_upload, dir: dir, content_type: "form")
     binary_content = form_upload.form.download
 
     client.create_block_blob(container, filename, binary_content)
@@ -46,6 +62,15 @@ module AzureHelper
 
   def self.send_failure_email(form, content)
     FormSubmissionMailer.email_form(form, content, form.user).deliver_now
+  end
+
+  def self.send_images_with_form(client, form, container, dir)
+    form.images.each_with_index do |img, index|
+      img_binary = img.download
+
+      filename = generate_filename(form: form, dir: dir, content_type: "image", index: index) 
+      client.create_block_blob(container, filename, img_binary)
+    end
   end
 end
     
