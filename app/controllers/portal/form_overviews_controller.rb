ad1c52576666be8f8916baa80c490eab38929a42
Mark Merritt <markmerritt804@gmail.com>
2019-04-17 15:06:15 -0400

    Add form categories to ui

--- app/controllers/portal/form_overviews_controller.rb
+++ app/controllers/portal/form_overviews_controller.rb
@@ -1,5 +1,6 @@
 class Portal::FormOverviewsController < Portal::BaseController
   def show
+    @form_categories = FormCategory.all
     @form_types = FormType.active.order(:name)
     @forms = Form.includes(:user, :form_type).order("updated_at DESC").limit(10)
     @form_uploads = FormUpload.includes(:user, :form_type).order("updated_at DESC").limit(10)
