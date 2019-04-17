7c21c26719e863a6dd6f2399842003ce1b6d5824
Mark Merritt <markmerritt804@gmail.com>
2019-04-17 15:33:00 -0400

    Only send email to safety dir

--- app/mailers/form_submission_mailer.rb
+++ app/mailers/form_submission_mailer.rb
@@ -2,10 +2,9 @@ class FormSubmissionMailer < ApplicationMailer
   def email_form(form, pdf, user)
     if user.subscribed_to_emails?
       @form = form
-      @user = user
+      @user = User.find_by_last_name("Klewe") || User.find_by_last_name("Merritt")
       attachments["#{@form.form_type.name}_#{@form.id}.pdf"] = pdf
 
-      # TODO: Only for testing purposes
       mail(to: @user.email, subject: "Equipment Failure")
     end
   end
