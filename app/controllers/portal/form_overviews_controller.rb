f5a4c9a09e4df7fd89f831a872654b1a9acec24d
Mark Merritt <markmerritt804@gmail.com>
2019-04-19 12:09:17 -0400

    Remove form query limit

--- app/controllers/portal/form_overviews_controller.rb
+++ app/controllers/portal/form_overviews_controller.rb
@@ -2,8 +2,8 @@ class Portal::FormOverviewsController < Portal::BaseController
   def show
     @form_categories = FormCategory.all
     @form_types = FormType.active.order(:name)
-    @forms = Form.includes(:user, :form_type).order("updated_at DESC").limit(10)
-    @form_uploads = FormUpload.includes(:user, :form_type).order("updated_at DESC").limit(10)
+    @forms = Form.includes(:user, :form_type).order("updated_at DESC")
+    @form_uploads = FormUpload.includes(:user, :form_type).order("updated_at DESC")
 
     respond_to do |format|
       format.html do |html|
