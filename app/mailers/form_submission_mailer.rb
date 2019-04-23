d7ed03de5d78d1be68aa98e5befef3c12bac18e6
Mark Merritt <markmerritt804@gmail.com>
2019-04-23 17:24:39 -0400

    Testing email image attachments

--- app/mailers/form_submission_mailer.rb
+++ app/mailers/form_submission_mailer.rb
@@ -1,11 +1,22 @@
+# TODO: 
+# - No need to pass in user
+# - Check if SD is subscribed to emails
+# - RAM consumption for forms with many attached images
+
 class FormSubmissionMailer < ApplicationMailer
   def email_form(form, pdf, user)
-    if user.subscribed_to_emails?
-      @form = form
-      @user = User.find_by_last_name("Klewe") || User.find_by_last_name("Merritt")
-      attachments["#{@form.form_type.name}_#{@form.id}.pdf"] = pdf
+    @form = form
+    # TODO: Remove (for testing purposes only)
+    # @user = User.find_by_last_name("Klewe") || User.find_by_last_name("Merritt")
+    @user = User.find_by_last_name("Merritt")
+    attachments["#{@form.form_type.name}_#{@form.id}.pdf"] = pdf
 
-      mail(to: @user.email, subject: "Equipment Failure")
+    if @form.images.attached?
+      @form.images.each_with_index do |img, index|
+        attachments["img_#{index}"] = img.blob.download
+      end
     end
+
+    mail(to: @user.email, subject: "Equipment Failure")
   end
 end
