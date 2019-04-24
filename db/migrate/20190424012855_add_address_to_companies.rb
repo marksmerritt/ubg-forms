bdc2ebfcfa60baa3514d772839420e7336b7a16c
Mark Merritt <markmerritt804@gmail.com>
2019-04-23 22:31:12 -0400

    Fix migration issue

--- db/migrate/20190424012855_add_address_to_companies.rb
+++ db/migrate/20190424012855_add_address_to_companies.rb
@@ -1,5 +1,7 @@
 class AddAddressToCompanies < ActiveRecord::Migration[5.2]
   def change
-    add_reference :companies, :address, foreign_key: true
+    if Rails.env.development?
+      add_reference :companies, :address, foreign_key: true
+    end
   end
 end
