01813bae6df08cd2bf68f9ee33057cd29d28162e
Mark Merritt <markmerritt804@gmail.com>
2019-04-30 19:06:57 -0400

    Fix N+1 query for form images

--- app/controllers/portal/form_overviews_controller.rb
+++ app/controllers/portal/form_overviews_controller.rb
@@ -2,8 +2,8 @@ class Portal::FormOverviewsController < Portal::BaseController
   def show
     @form_categories = FormCategory.all
     @form_types = FormType.active.order(:name)
-    @forms = Form.includes(:user, :form_type).order("updated_at DESC")
-    @form_uploads = FormUpload.includes(:user, :form_type).order("updated_at DESC")
+    @forms = Form.includes(:user, :form_type).with_attached_images.order("created_at DESC").limit(50)
+    @form_uploads = FormUpload.includes(:user, :form_type).order("created_at DESC")
 
     respond_to do |format|
       format.html do |html|
