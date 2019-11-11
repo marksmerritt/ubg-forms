df125cd52035dc7c79703c0c0677f463a4fff22a
Mark Merritt <markmerritt804@gmail.com>
2019-11-11 02:45:06 -0500

    Stop checking for form field presence

--- app/models/form.rb
+++ app/models/form.rb
@@ -9,14 +9,17 @@ class Form < ApplicationRecord
 
   
   validates_presence_of :job_number
-  validate :all_fields_present
+  # validate :all_fields_present
   validate :image_limit
 
-  def all_fields_present
-    if has_empty_fields?(header_properties) || (form_type.has_checklist? && has_empty_fields?(checklist_properties)) || has_empty_fields?(footer_properties)
-      errors.add(:base, "Please fill out all fields")
-    end
-  end
+
+  # There is no need to check for incomplete fields. If this changes, simply uncomment the all_fields_present method and the validate :all_fields_present call.  
+
+  # def all_fields_present
+  #   if has_empty_fields?(header_properties) || (form_type.has_checklist? && has_empty_fields?(checklist_properties)) || has_empty_fields?(footer_properties)
+  #     errors.add(:base, "Please fill out all fields")
+  #   end
+  # end
 
   def has_empty_fields?(fields)
     fields.values.any?{ |field| field.empty? }
